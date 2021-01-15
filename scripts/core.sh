830ef5ebccd3d29424364a626b6b47b0004e8614
Jaxon <mohamedmostels@gmail.com>
2021-01-15 21:33:03 +0200

    Add cks-tools role

--- scripts/core.sh
+++ scripts/core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "TASK [1] Install core utilities"
-apt-get install -y ansible bash-completion etcd-client sshpass vim apt-transport-https
+apt-get install -y ansible bash-completion etcd-client sshpass vim apt-transport-https ca-certificates software-properties-common wget gnupg lsb-release
 
 echo "TASK [2] Run Kluster role"
 ansible-playbook -b /vagrant/Kluster.yml
